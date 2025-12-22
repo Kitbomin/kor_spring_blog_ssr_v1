4726f999c48787180bf12be32c7ff91b81025470
Kitbomin <zzxxcc1433@naver.com>
2025-12-22 11:15:56 +0900

    adminInterceptor

--- src/main/java/org/example/demo_ssr_v1_1/admin/AdminController.java
+++ src/main/java/org/example/demo_ssr_v1_1/admin/AdminController.java
@@ -1,8 +1,6 @@
 package org.example.demo_ssr_v1_1.admin;
 
 import jakarta.servlet.http.HttpSession;
-import org.example.demo_ssr_v1_1._core.errors.exception.Exception401;
-import org.example.demo_ssr_v1_1._core.errors.exception.Exception403;
 import org.example.demo_ssr_v1_1.user.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,14 +15,7 @@ public class AdminController {
 
         User sessionUser = (User) session.getAttribute("sessionUser");
 
-        if (sessionUser == null) {
-            throw new Exception401("로그인 필요");
-        }
-
-        if (!sessionUser.isAdmin()) {
-            throw new Exception403("접근 권한 없음");
-        }
-
+        model.addAttribute("user", sessionUser);
         return "admin/dashboard";
     }
 
